dbc6c6a3d17e60d4905d727ebbe9ad2761618922
zhaojunfei <773152@qq.com>
2017-11-22 17:35:26 +0800

    处理结果值得

--- src/cn/forgeeks/domain/Statistics.java
+++ src/cn/forgeeks/domain/Statistics.java
@@ -1,5 +1,17 @@
 package cn.forgeeks.domain;
 
 public class Statistics {
-
+	private String studentId;
+	private String studentName;
+	private String studentPassword;
+	private String sex;
+	private String classId;
+	private String className;
+	private String status;
+	private String bedroomId;
+	private String bedroomName;
+	private String studentNo;
+	private String grade;
+	private String collegeId;
+	private String collegeName;
 }
